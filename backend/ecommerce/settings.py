bc7f0156ef34cb10ef4811f496c0176b2fd8dd47
MonishaJ1997 <jkmonisha97@gmail.com>
2025-09-18 14:10:43 +0530

    Update settings.py

--- backend/ecommerce/settings.py
+++ backend/ecommerce/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,6 +126,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # ✅ ADD THIS
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
@@ -194,5 +198,6 @@ SIMPLE_JWT = {
     "BLACKLIST_AFTER_ROTATION": True,
     "AUTH_HEADER_TYPES": ("Bearer",),
 }
+
 
 
